5f3f70d2218048015cc8a0a079a6c12b9bc7e1cd
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-07-09 22:46:38 +0900

    migration完了

--- backend/db/schemas/schema_migrations.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `schema_migrations` (
-  `version` bigint NOT NULL,
-  `dirty` tinyint(1) NOT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
